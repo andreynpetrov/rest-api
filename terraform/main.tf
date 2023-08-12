57f99b38b61410ec83232a68f4c4834e516988e0
Andrey Petrov <a.petrov@aligntech.com>
2023-08-13 00:39:16 +0200

    Switch to https module source.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 module "lambda-rest-api" {
-  source                  = "git@github.com:Smartbrood/terraform-aws-lambda.git?ref=release/v1.0.0"
+  source                  = "github.com/Smartbrood/terraform-aws-lambda.git?ref=release/v1.0.0"
   name                    = "rest-api-dev"
   aws_ecr_repository_name = "rest-api"
   image_tag               = "edge"
